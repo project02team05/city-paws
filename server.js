201af7863f7a3f51ae47833af8383e1072e94ee1
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-09-25 12:19:57 -0400

    initial handlebars setup

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 // Dependencies
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const express = require("express");
+//const routes = require("./controllers");
+//const sequelize = require("./config/connection");
 
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.listen(3001, () => console.log("Server running on 3001"));
