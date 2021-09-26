2bd659bceeaf972a27d595902cb85efd8ce75b14
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-26 15:32:32 -0400

    updated add-comment.js

--- public/javascript/add-comment.js
+++ public/javascript/add-comment.js
@@ -2,16 +2,16 @@ async function commentFormHandler(event) {
   event.preventDefault();
 
   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-  const post_id = window.location.toString().split('/')[
+  const service_id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
 
   if (comment_text) {
-    const response = await fetch('/api/comments', {
+    const response = await fetch('/api/reviews', {
       method: 'POST',
       body: JSON.stringify({
-        post_id,
-        comment_text
+        service_id,
+        comment
       }),
       headers: {
         'Content-Type': 'application/json'
