8fecfdbbf36f919b3a52f5181df7513a395aa8bf
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-09-27 20:14:06 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-
 const express = require("express");
-const routes = require('./controllers');
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 require("dotenv").config();
 const path = require("path");
@@ -13,7 +12,6 @@ const hbs = exphbs.create({}); // [add 'helpers' in the bracket after]
 // set up express-session & sequelize store:
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -30,6 +28,15 @@ const sess = {
 
 app.use(session(sess));
 
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
 
 app.get("/signup", (req, res) => {
   res.render("signup");
@@ -39,12 +46,16 @@ app.use(express.urlencoded({ extended: true }));
 // Static assets folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
-
