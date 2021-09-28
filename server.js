b191d2d98dcd80f53c46cc3d882012744650785c
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-27 23:02:07 -0400

    fixed/replaced hbs code in server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+const express = require('express');
+const routes = require('./controllers'); 
+const sequelize = require('./config/connection');
+// require 'path' for static folder below
+const path = require('path');
 
-const express = require("express");
-const routes = require("./controllers/index");
-const sequelize = require("./config/connection");
-require("dotenv").config();
-const path = require("path");
+// add hbs helper functions [uncomment this after]
+const helpers = require('./utils/helpers');
 
 // set up Handlebars.js as your app's template engine of choice & add helper functions:
 const exphbs = require("express-handlebars");
-
-const hbs = exphbs.create({}); // [add 'helpers' in the bracket after]
+const hbs = exphbs.create({helpers}); // [add 'helpers' in the bracket after]
 
 // set up express-session & sequelize store:
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -30,21 +32,31 @@ const sess = {
 
 app.use(session(sess));
 
-
-app.get("/signup", (req, res) => {
-  res.render("signup");
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Static assets folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
 
 app.use(routes);
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
-
