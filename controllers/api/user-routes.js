353e6137cf67dd5b7c2d3161bb33cd75127f065a
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-27 23:24:26 -0400

    added login/logout routes and working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,6 +54,7 @@ router.get("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
 // Post user
 router.post("/", (req, res) => {
   // expects Login information
@@ -77,6 +78,48 @@ router.post("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+// user login
+router.post('/login', (req, res) => {
+  // expects {email: 'testing@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  });
+});
+
+// user logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 // Export the Router
