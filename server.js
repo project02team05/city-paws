0e26e2054a2da45baa6d21e9ba76d9bc384c667b
Abrar Chowdhry <abrarchowdhry44@hotmail.com>
2021-09-28 18:01:54 -0400

    Updated the server.js file for front end

--- server.js
+++ server.js
@@ -31,12 +31,20 @@ const sess = {
 app.use(session(sess));
 
 
-app.get("/signup", (req, res) => {
-  res.render("signup");
+app.get("/home", (req, res) => {
+  res.render("home");
 });
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 app.use(express.json());
