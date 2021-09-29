b76345c5a9b618b4e0a5ef6e69452e3f5e33db5f
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-09-28 21:40:14 -0400

    updated  home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,38 +1,19 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Service, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
     console.log('======================');
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at',
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
+    Service.findAll({
+        
+      
     })
         .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
+            const service = dbPostData.map(service => service.get({ plain: true }));
 
-            res.render('homepage', {
-                posts,
+            res.render('home', {
+                service,
                 loggedIn: req.session.loggedIn
             });
         })
