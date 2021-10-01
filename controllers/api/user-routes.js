63f244041bf648d6b2928a6f41a507a7f75602de
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-10-01 18:02:41 -0400

    linked signup to the backend

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,52 +35,53 @@ router.post("/login", async (req, res) => {
 
       res.json(dbUserData);
     });
-    res.redirect("/home");
+    res.redirect("/search");
   } else {
     res.redirect("/login?status=failed");
   }
 });
 
 // Get User by Id
-router.get("/:id", (req, res) => {
-  User.findOne({
-    attributes: { exclude: ["password"] },
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ["id", "title", "post_url", "created_at"],
-      },
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "created_at"],
-        include: {
-          model: Post,
-          attributes: ["title"],
-        },
-      },
-      {
-        model: Post,
-        attributes: ["title"],
-        through: Vote,
-        as: "voted_posts",
-      },
-    ],
-  })
-    .then((dbUserData) => {
-      if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+// router.get("/:id", (req, res) => {
+//   User.findOne({
+//     attributes: { exclude: ["password"] },
+//     where: {
+//       id: req.params.id,
+//     },
+//     include: [
+//       {
+//         model: Post,
+//         attributes: ["id", "title", "post_url", "created_at"],
+//       },
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "created_at"],
+//         include: {
+//           model: Post,
+//           attributes: ["title"],
+//         },
+//       },
+//       {
+//         model: Post,
+//         attributes: ["title"],
+//         through: Vote,
+//         as: "voted_posts",
+//       },
+//     ],
+//   })
+//     .then((dbUserData) => {
+//       if (!dbUserData) {
+//         res.status(404).json({ message: "No user found with this id" });
+//         return;
+//       }
+//       res.json(dbUserData);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
+
 // Post user
 router.post("/", (req, res) => {
   // expects Login information
@@ -106,35 +107,35 @@ router.post("/", (req, res) => {
     });
 });
 
-// user login
-router.post('/login', (req, res) => {
-  // expects {email: 'testing@gmail.com', password: 'password1234'}
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  }).then(dbUserData => {
-    if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
-      return;
-    }
+// // user login
+// router.post('/login', (req, res) => {
+//   // expects {email: 'testing@gmail.com', password: 'password1234'}
+//   User.findOne({
+//     where: {
+//       email: req.body.email
+//     }
+//   }).then(dbUserData => {
+//     if (!dbUserData) {
+//       res.status(400).json({ message: 'No user with that email address!' });
+//       return;
+//     }
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
+//     const validPassword = dbUserData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
-      return;
-    }
+//     if (!validPassword) {
+//       res.status(400).json({ message: 'Incorrect password!' });
+//       return;
+//     }
 
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
+//     req.session.save(() => {
+//       req.session.user_id = dbUserData.id;
+//       req.session.username = dbUserData.username;
+//       req.session.loggedIn = true;
   
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
-  });
-});
+//       res.json({ user: dbUserData, message: 'You are now logged in!' });
+//     });
+//   });
+// });
 
 // user logout
 router.post('/logout', (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,40 +3,31 @@ const sequelize = require('../config/connection');
 const { Service, User, Comment } = require('../models');
 
 // get all posts for homepage
-router.get('/', (req, res) => {
+router.get('/home', (req, res) => {
     console.log('======================');
-<<<<<<< HEAD
-    Service.findAll({
-        
-      
-    })
-        .then(dbPostData => {
-            const service = dbPostData.map(service => service.get({ plain: true }));
-
-            res.render('home', {
-                service,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-=======
     
     res.render("home");
 
->>>>>>> a330b34b4b7fcb54081dc53c8251996d0ff45733
 });
 
-
+// login page 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/');
+        res.redirect('/search');
         return;
     }
 
     res.render('login');
 });
+
+// signup page 
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/search');
+      return;
+    }
+  
+    res.render('signup');
+  });
 
 module.exports = router;
