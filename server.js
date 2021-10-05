68a8277eda233ae11396a7ce409a1b9602d43866
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-10-04 23:02:52 -0400

    fixed logo and hero image

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 const sess = {
   secret: process.env.SESSION_SECRET,
